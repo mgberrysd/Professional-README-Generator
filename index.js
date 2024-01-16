d58b91c7938852f35b9366e63ce33c69b22c0ecc
mgberrysd <mgberrysd@gmail.com>
2024-01-16 15:57:04 -0800

    fixed formatting of README in template literal

--- index.js
+++ index.js
@@ -10,95 +10,93 @@ inquirer
       message: 'What is your project name?',
     },
     {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a short description of your project:',
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'What steps are required to install your project?',
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'How does the user use your project?',
-      },
-      {
-        type: 'input',
-        name: 'features',
-        message: 'What features does this project have?',
-      },
-      {
-        type: 'input',
-        name: 'contribute',
-        message: 'How can others contribute to this project?',
-      },
-      {
-        type: 'input',
-        name: 'tests',
-        message: 'What tests were performed?',
-      {
-        type: 'input',
-        name: 'credits',
-        message: 'Who contributed to this project?',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'What license does this project use?',
-        choices: ['None','Apache-2.0','GPL-3.0','MIT','BSD-2-Clause','BSD-3-Clause','BSL-1.0','CC0-1.0','EPL-2.0','AGPL-3.0','GPL-2.0','LGPL-2.1','MPL-2.0','Unlicense']
-      },
-
-      },
+      type: 'input',
+      name: 'description',
+      message: 'Enter a short description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What steps are required to install your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How does the user use your project?',
+    },
+    {
+      type: 'input',
+      name: 'features',
+      message: 'What features does this project have?',
+    },
+    {
+      type: 'input',
+      name: 'contribute',
+      message: 'How can others contribute to this project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What tests were performed?',
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Who contributed to this project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license does this project use?',
+      choices: ['None', 'Apache-2.0', 'GPL-3.0', 'MIT', 'BSD-2-Clause', 'BSD-3-Clause', 'BSL-1.0', 'CC0-1.0', 'EPL-2.0', 'AGPL-3.0', 'GPL-2.0', 'LGPL-2.1', 'MPL-2.0', 'Unlicense']
+    },
   ])
   .then((data) => {
-    const readme = `
-    # <${data.name}>
+    const readme = `# ${data.name}
 
-    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
 
-    ## Description
+## Description
+
+${data.description}
     
-    ${data.description}
-    
-    ## Table of Contents
+## Table of Contents
         
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Features](#features)
-    - [How to Contribute](#how)
-    - [Tests](#test)
-    - [Credits](#credits)
-    - [License](#license)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [How to Contribute](#how)
+- [Tests](#test)
+- [Credits](#credits)
+- [License](#license)
     
-    ## Installation
+## Installation
     
-    ${data.installation}
+${data.installation}
     
-    ## Usage
+## Usage
     
-    ${data.usage}
+${data.usage}
 
-    ## Features
+## Features
     
-    ${data.features}
+${data.features}
     
-    ## How to Contribute
+## How to Contribute
     
-    ${data.contribute}
+${data.contribute}
     
-    ## Tests
+## Tests
     
-    ${data.tests}
+${data.tests}
 
-    ## Credits
+## Credits
     
-    ${data.credits}
+${data.credits}
     
-    ## License
+## License
     
-    ${data.license}
+${data.license}
  
     `;
 
---
+++ test.md
@@ -0,0 +1,47 @@
+# ReadME Gen
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+## Description
+
+asfghhn
+    
+## Table of Contents
+        
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [How to Contribute](#how)
+- [Tests](#test)
+- [Credits](#credits)
+- [License](#license)
+    
+## Installation
+    
+asghnoagon
+    
+## Usage
+    
+awrioahgnoams,
+
+## Features
+    
+aspnao231
+    
+## How to Contribute
+    
+acm,asigp[
+    
+## Tests
+    
+asnpx,-1-
+
+## Credits
+    
+asoljmcpil
+    
+## License
+    
+None
+ 
+    
